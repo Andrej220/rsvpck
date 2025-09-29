599500d6ca7cd7790f4e1a08bd432b8fa666a8e8
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-09-29 12:06:00 +0200

    add argument parsing to display version, add variable version, make connectivity test ip an variable to change it at build time if needed, add const - application name

--- rsvpck.go
+++ rsvpck.go
@@ -2,12 +2,17 @@ package main
 
 import(
 	"time"
+    "fmt"
+    "os"
 )
 
 const (
-	internetConnectivityTestIP = "8.8.8.8:53"
     timeout = 5 * time.Second
+    applicationName = "RSvP connectivity checker"
 )
+
+var version = "dev"
+var internetConnectivityTestIP = "8.8.8.8:53"
 
 var endpoints = []string{
                 "https://insite-eu.gehealthcare.com",
@@ -21,7 +26,7 @@ var proxies = []string{
 
 func RunRSVPDiagnostics(config *NetTestConfig) []NetTestResult {
     var results []NetTestResult
-    
+
     results = append(results, testInternetConnectivity(config))
     for _, s := range(config.CheckEndpoints){
         results = append(results, testDNSResolution(s))
@@ -39,6 +44,13 @@ func RunRSVPDiagnostics(config *NetTestConfig) []NetTestResult {
 }
 
 func main(){
+
+    for _, arg := range os.Args[1:] {
+    	if arg == "-v" || arg == "--version" || arg == "-version" {
+    		fmt.Printf("%s version %s\n", applicationName, version)
+    		return
+    	}
+    }
 
     config := &NetTestConfig{
             SiteID:  getMachineUUID(),
