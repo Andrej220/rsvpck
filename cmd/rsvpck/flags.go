b7755bcedd7de51d0220f4d3585b2ab71dda5593
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-10-18 14:29:54 +0200

    feat: add config struct to handle falgs: print version, force ASCII, force rendering

--- cmd/rsvpck/flags.go
+++ cmd/rsvpck/flags.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"flag"
+
 )
 
 type rsvpckConf struct {
-	textRender  bool
-	tableRender bool
-	forseASCII bool
+	textRender  	bool
+	tableRender 	bool
+	forseASCII 		bool
+	speedtest  		bool
+	printVersion	bool
 }
 
 func NewRsvpckConf() rsvpckConf {
@@ -35,10 +38,14 @@ func (r *rsvpckConf) SetRender(textRender bool) {
 func parseFlagsToConfig() *rsvpckConf {
 	txtRender := flag.Bool("text", false, "render connectivity info as text. Default table")
 	flagForceASCII := flag.Bool("ascii", false, "Force ASCII-only output (no Unicode symbols)")
+	speedtestFlag := flag.Bool("speedtest", false, "Run optional speedtest")
+	printVersion := flag.Bool("version", false, "Print version")
 	flag.Parse()
 
 	r := NewRsvpckConf()
 	r.SetRender(*txtRender)
 	r.forseASCII = *flagForceASCII
+	r.speedtest = *speedtestFlag
+	r.printVersion = *printVersion
 	return &r
 }
