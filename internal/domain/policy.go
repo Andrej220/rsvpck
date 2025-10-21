529fca906157a64c6bcf65c1898533fd1cb74cff
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-10-21 22:46:37 +0200

    refactor: use map for policy

--- internal/domain/policy.go
+++ internal/domain/policy.go
@@ -7,14 +7,35 @@ const (
 	PolicyExhaustive
 )
 
-var policyNames = [...]string{
-	"Optimized",
-	"Exhaustive",
+var policyToString = map[ExecutionPolicy]string{
+	PolicyOptimized: "Optimized",
+	PolicyExhaustive: "Exhaustive",
+}
+
+var stringToPolicy map[string]ExecutionPolicy
+
+func init() {
+	stringToPolicy = make(map[string]ExecutionPolicy)
+	for policy, str := range policyToString {
+		stringToPolicy[str] = policy
+	}
 }
 
 func (e ExecutionPolicy) String() string {
-	if int(e) < len(policyNames) {
-		return policyNames[e]
+	if str, exists := policyToString[e]; exists {
+		return str
 	}
 	return "Unknown"
+}
+
+func ParseExecutionPolicy(s string) (ExecutionPolicy, bool) {
+	policy, exists := stringToPolicy[s]
+	return policy, exists
+}
+
+func ParseExecutionPolicyWithDefault(s string) ExecutionPolicy {
+	if policy, exists := stringToPolicy[s]; exists {
+		return policy
+	}
+	return PolicyOptimized 
 }
