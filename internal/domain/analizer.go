0f13ee8378affa7d59c72fb7a047ae2322b1557b
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-10-21 22:44:59 +0200

    refactor: move analize to result instance

--- internal/domain/analizer.go
+++ internal/domain/analizer.go
@@ -1,64 +1,15 @@
 package domain
 
-func AnalyzeConnectivity(probes []Probe, config NetTestConfig) ConnectivityResult {
-	var (
-		vpnOK      bool
-		directOK   bool
-		proxyOK    bool
-		dnsOK bool
-	)
+import (
+	"time"
+)
 
-	for _, p := range probes {
-		if p.IsDNSProbe() && p.IsSuccessful() {
-			dnsOK = true
-			break
-		}
+func AnalyzeConnectivity(probes []Probe, _ NetTestConfig) ConnectivityResult {
+	r := ConnectivityResult{
+		Probes:    probes,
+		Timestamp: time.Now(),
 	}
-
-	for _, p := range probes {
-		if !p.IsSuccessful() {
-			continue
-		}
-
-		switch {
-		case p.Endpoint.IsVPN():
-			vpnOK = true
-
-		case !p.Endpoint.IsPublic():
-			continue
-
-		case p.Endpoint.IsDirectType():
-			directOK = true
-
-		case p.Endpoint.IsProxyType():
-			proxyOK = true
-		}
-	}
-
-	var mode ConnectivityMode
-	switch {
-	case directOK && dnsOK:
-		mode = ModeDirect
-	case proxyOK:
-		mode = ModeViaProxy
-	case vpnOK:
-		mode = ModeViaVPN
-	default:
-		mode = ModeNone
-	}
-
-	return NewConnectivityResult(mode, probes)
+	r.DetermineMode()
+	return r
 }
 
-func buildSummary(mode ConnectivityMode) string {
-	switch mode {
-	case ModeViaVPN:
-		return "Connected via VPN."
-	case ModeDirect:
-		return "Direct internet."
-	case ModeViaProxy:
-		return "Internet via proxy"
-	default:
-		return "No connection"
-	}
-}
--- internal/domain/result.go
+++ internal/domain/result.go
@@ -41,3 +41,63 @@ func (r ConnectivityResult) FailedProbes() []Probe {
 	}
 	return failed
 }
+
+func (r *ConnectivityResult) DetermineMode() {
+	var (
+		vpnOK, directOK, proxyOK, dnsOK bool
+	)
+
+	for _, p := range r.Probes {
+		if p.IsDNSProbe() && p.IsSuccessful() {
+			dnsOK = true
+			break
+		}
+	}
+
+	for _, p := range r.Probes {
+		if !p.IsSuccessful() {
+			continue
+		}
+
+		switch {
+		case p.Endpoint.IsVPN():
+			vpnOK = true
+
+		case !p.Endpoint.IsPublic():
+			continue
+
+		case p.Endpoint.IsDirectType():
+			directOK = true
+
+		case p.Endpoint.IsProxyType():
+			proxyOK = true
+		}
+	}
+
+	switch {
+	case directOK && dnsOK:
+		r.Mode = ModeDirect
+	case proxyOK:
+		r.Mode = ModeViaProxy
+	case vpnOK:
+		r.Mode = ModeViaVPN
+	default:
+		r.Mode = ModeNone
+	}
+
+	r.IsConnected = r.Mode.IsConnected()
+	r.Summary = buildSummary(r.Mode)
+}
+
+func buildSummary(mode ConnectivityMode) string {
+	switch mode {
+	case ModeViaVPN:
+		return "Connected via VPN."
+	case ModeDirect:
+		return "Direct internet."
+	case ModeViaProxy:
+		return "Internet via proxy"
+	default:
+		return "No connection"
+	}
+}
